d77fb016c3f746a67f3e58bf3194ad20dd4fca6e
Cjverity01 <136152652+Cjverity01@users.noreply.github.com>
2024-11-16 20:23:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -275,4 +275,4 @@ Modmail Guild ID: {modmail_guild_id if modmail_guild_id else 'N/A'}
     ''')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
